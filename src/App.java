71b93dcaeea8cdc2daa890033d5e9ac15245a56c
Morad <460583232@sharkia1.moe.edu.eg>
2022-11-08 15:53:59 +0200

    Fixing

--- src/App.java
+++ src/App.java
@@ -3,12 +3,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class App {
-    static int MaxQ = 1000;
-    static int MaxX = 1000;
+    static int MaxQ = 50;
+    static int MaxX = 100;
     static int MIPL = 20; // Optimal depth, do NOT edit
 
     public static void main(String[] args) throws Exception {
-        ExecutorService ES = Executors.newCachedThreadPool();
+        ExecutorService ES = Executors.newFixedThreadPool(10);
         for (int i = 1; i < MaxQ; i += 2) { // Odd 'q's only!
             if(
                 !Utils.isInt(Math.log(i+1) / Utils.log2)
--- src/Worker.java
+++ src/Worker.java
@@ -15,37 +15,43 @@ public class Worker implements Runnable {
     @Override
     public void run() {
         for (int x = 3; x < MaxX; x += 2) {
-            if(check(x)) {
-            for(int a = 0;a < MIPL; a++) {
-                double X_0 = (q*(x) + 1) / (Utils.TwoPow(a));
-                if(X_0 == x && !results.contains(x)) results.add(x);
-                
-                for(int b = 0;b < MIPL; b++) {
-                    double X_1 = (q*(X_0) + 1) / (Utils.TwoPow(b));
-                    if(X_1 == x && !results.contains(x)) results.add(x);
+            if (check(x)) {
+                for (int a = 0; a < MIPL; a++) {
+                    double X_0 = (q * (x) + 1) / (Utils.TwoPow(a));
+                    if (X_0 == x && !results.contains(x))
+                        results.add(x);
 
-                    for(int c = 0;c < MIPL; c++) {
-                        double X_2 = (q*(X_1) + 1) / (Utils.TwoPow(c));
-                        if(X_2 == x && !results.contains(x)) results.add(x);
+                    for (int b = 0; b < MIPL; b++) {
+                        double X_1 = (q * (X_0) + 1) / (Utils.TwoPow(b));
+                        if (X_1 == x && !results.contains(x))
+                            results.add(x);
 
-                        for(int d = 0; d < MIPL; d++) {
-                            double X_3 = (q*(X_2) + 1) / (Utils.TwoPow(d));
-                            if(X_3 == x && !results.contains(x)) results.add(x);
+                        for (int c = 0; c < MIPL; c++) {
+                            double X_2 = (q * (X_1) + 1) / (Utils.TwoPow(c));
+                            if (X_2 == x && !results.contains(x))
+                                results.add(x);
 
-                            for(int e = 0; e < MIPL; e++) {
-                                double X_4 = (q*(X_3) + 1) / (Utils.TwoPow(e));
-                                if(X_4 == x && !results.contains(x)) results.add(x);
-                                
-                                for(int f = 0; f < MIPL; f++) {
-                                    double X_5 = (q*(X_4) + 1) / (Utils.TwoPow(f));
-                                    if(X_5 == x && !results.contains(x)) results.add(x);
+                            for (int d = 0; d < MIPL; d++) {
+                                double X_3 = (q * (X_2) + 1) / (Utils.TwoPow(d));
+                                if (X_3 == x && !results.contains(x))
+                                    results.add(x);
+
+                                for (int e = 0; e < MIPL; e++) {
+                                    double X_4 = (q * (X_3) + 1) / (Utils.TwoPow(e));
+                                    if (X_4 == x && !results.contains(x))
+                                        results.add(x);
+
+                                    for (int f = 0; f < MIPL; f++) {
+                                        double X_5 = (q * (X_4) + 1) / (Utils.TwoPow(f));
+                                        if (X_5 == x && !results.contains(x))
+                                            results.add(x);
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
         }
         System.out.println("For q=" + q + ": " + results);
     }
