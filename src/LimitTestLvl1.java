1cb351ff8ad9b7e88b668781e191713b7fef4e1c
Morad <460583232@sharkia1.moe.edu.eg>
2022-11-09 05:59:54 +0200

    0

--- src/LimitTestLvl1.java
+++ src/LimitTestLvl1.java
@@ -4,6 +4,7 @@ public class LimitTestLvl1 {
         for(int q = 1;q < 20;q += 2) {
             for(int a = 1; a < 20; a++) {
                 double B = Math.log(((x * Utils.XPow(q, 2)) + q + Utils.TwoPow(a)) / (x)) / Utils.log2;
+                B += -a;
                 for(int it = 0; it < Math.ceil(B); it++) {
                     double X = (-Utils.TwoPow(a) - q) / (-Utils.TwoPow(a+it) + Utils.XPow(q, 2));
                     if((int) X == X) System.out.println("For q="+q+", x="+X);
