c40676ff96b65a310cff3761ef1f5dfaf3ec2e96
Morad <460583232@sharkia1.moe.edu.eg>
2022-11-09 05:56:15 +0200

    LvlLoop

--- src/LimitTest.java
+++ src/LimitTest.java
@@ -1,7 +1,7 @@
 public class LimitTest {
     public static void main(String[] args) {
         double x = 2e100 + 1;
-        for(int q = 1;q < 9;q += 1) {
+        for(int q = 1;q < 9;q += 2) {
             double A = Math.log((x*(q)+1)/(x)) / Utils.log2;
             if(A>=1)System.out.println("There exists "+Math.ceil(A)+" Solutions for "+q);
         }
---
+++ src/LimitTestLvl1.java
@@ -0,0 +1,14 @@
+public class LimitTestLvl1 {
+    public static void main(String[] args) {
+        double x = 2e100 + 1;
+        for(int q = 1;q < 20;q += 2) {
+            for(int a = 1; a < 20; a++) {
+                double B = Math.log(((x * Utils.XPow(q, 2)) + q + Utils.TwoPow(a)) / (x)) / Utils.log2;
+                for(int it = 0; it < Math.ceil(B); it++) {
+                    double X = (-Utils.TwoPow(a) - q) / (-Utils.TwoPow(a+it) + Utils.XPow(q, 2));
+                    if((int) X == X) System.out.println("For q="+q+", x="+X);
+                }
+            }
+        }
+    }
+}
