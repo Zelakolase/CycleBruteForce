d9398cb60d537a7ba8cab5d402a2a614e513ee0b
Morad <460583232@sharkia1.moe.edu.eg>
2022-11-10 13:02:41 +0200

    THE ULTIMATE TEST

--- src/ConfigurationForLimitTest.java
+++ src/ConfigurationForLimitTest.java
@@ -1,6 +1,6 @@
 public class ConfigurationForLimitTest {
-    public static int MIPL = 50;
+    public static int MIPL = 100;
     public static int MinQ = 3;
-    public static int MaxQ = 10001;
-    public static double MaxX = 10001;
+    public static int MaxQ = 100_001;
+    public static double MaxX = 10_000_001;
 }
