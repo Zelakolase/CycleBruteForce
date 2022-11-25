4e6e3af86fbad047013ae71c4f6fdd34d9c1e6d7
Morad <460583232@sharkia1.moe.edu.eg>
2022-11-25 18:18:58 +0200

    0

--- src/lib/ConfigurationForLimitTest.java
+++ src/lib/ConfigurationForLimitTest.java
@@ -1,6 +1,6 @@
 package lib;
 public class ConfigurationForLimitTest {
-    public static int MinQ = 5;
-    public static int MaxQ = 7;
+    public static int MinQ = 3;
+    public static int MaxQ = 1001;
     public static double MaxX = 1_000_001d;
 }
